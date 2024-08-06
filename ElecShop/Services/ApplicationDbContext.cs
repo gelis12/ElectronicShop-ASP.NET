0f0ca4fbcef7427cb7d02f13b89e867eb6d892c9
Elis Galut <elis.galut96@gmail.com>
2024-08-06 11:34:30 +0300

    Add project files.

---
+++ ElecShop/Services/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ElecShop.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
