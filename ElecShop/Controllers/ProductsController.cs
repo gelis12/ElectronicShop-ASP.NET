0c82b89b811f051c168c97caa6e6af2df33a859c
Elis Galut <elis.galut96@gmail.com>
2024-08-06 16:57:06 +0300

    adding appDbContext, controller and view

---
+++ ElecShop/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using ElecShop.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElecShop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public IActionResult Index()
+        {
+            var products = _context.Products.OrderByDescending(p=>p.Id).ToList();
+            return View(products);
+        }
+    }
+}
--- ElecShop/Services/ApplicationDbContext.cs
+++ ElecShop/Services/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ElecShop.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ElecShop.Services
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Product> Products { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
