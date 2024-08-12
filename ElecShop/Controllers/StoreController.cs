53e2faded3f7863fd4d77d3656150de1faa96544
Elis Galut <elis.galut96@gmail.com>
2024-08-12 14:08:23 +0300

    Adding Store cintroller & view

---
+++ ElecShop/Controllers/StoreController.cs
@@ -0,0 +1,22 @@
+﻿using ElecShop.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElecShop.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public StoreController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.OrderByDescending(p => p.Id).ToList();
+
+            ViewBag.Products = products;
+
+            return View(products);
+        }
+    }
+}
