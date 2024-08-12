965ac14ad0c2aa446b3e21c2d4ad44caa3e45583
Elis Galut <elis.galut96@gmail.com>
2024-08-12 16:00:54 +0300

    Adding pagination functionality and products on view

--- ElecShop/Controllers/StoreController.cs
+++ ElecShop/Controllers/StoreController.cs
@@ -1,4 +1,5 @@
-﻿using ElecShop.Services;
+﻿using ElecShop.Models;
+using ElecShop.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ElecShop.Controllers
@@ -6,17 +7,35 @@ namespace ElecShop.Controllers
     public class StoreController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly int _totalpageItems = 8;
         public StoreController(ApplicationDbContext context)
         {
             _context = context;
         }
-        public IActionResult Index()
+        public IActionResult Index(int pageIndex)
         {
-            var products = _context.Products.OrderByDescending(p => p.Id).ToList();
+            IQueryable<Product> query = _context.Products;
 
+            query = query.OrderByDescending(p => p.Id);
+
+            
+
+            if (pageIndex < 1)
+            {
+                pageIndex = 1; 
+            }
+
+            decimal count = query.Count();
+            int totalPages = (int)Math.Ceiling(count / _totalpageItems);
+            query = query.Skip((pageIndex - 1) * _totalpageItems).Take(_totalpageItems);
+
+            var products = query.ToList();
+            
             ViewBag.Products = products;
+            ViewBag.PageIndex = pageIndex;
+            ViewBag.TotalPages = totalPages;
 
-            return View(products);
+            return View();
         }
     }
 }
