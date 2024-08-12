b5a593b56c9fb6f7fcd3bab11ecbda6eb432e5be
Elis Galut <elis.galut96@gmail.com>
2024-08-12 12:12:21 +0300

    moving products to Admin dropdown

--- ElecShop/Controllers/ProductsController.cs
+++ ElecShop/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ElecShop.Controllers
 {
+    [Route("/Admin/[controller]/{action=Index}/{id?}")]
     public class ProductsController : Controller
     {
         private readonly ApplicationDbContext _context;
