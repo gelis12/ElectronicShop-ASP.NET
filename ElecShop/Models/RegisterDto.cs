db3117caa430b0a9bca2268bd30443326edc2ede
Elis Galut <elis.galut96@gmail.com>
2024-08-13 13:31:28 +0300

    Adding Register model

---
+++ ElecShop/Models/RegisterDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElecShop.Models
+{
+    public class RegisterDto
+    {
+        [Required(ErrorMessage = "The First Name field is required"), MaxLength(100)]
+        public string FirstName { get; set; } = "";
+        [Required(ErrorMessage = "The Last Name field is required"), MaxLength(100)]
+        public string LastName { get; set; } = "";
+        [Required, EmailAddress, MaxLength(100)]
+        public string EmailAddress { get; set; } = "";
+        [Required(ErrorMessage = "The format of the Phone Number is not valid"), MaxLength(20)]
+        public string? PhoneNumber { get; set; }
+        [Required, MaxLength(200)]
+        public string Address { get; set; } = "";
+        [Required,MaxLength(100)]
+        public string Password { get; set; } = "";
+        [Required(ErrorMessage = "The Confirm Password field is required")]
+        [Compare("Password", ErrorMessage = "Confirm Password and Password do not match")]
+        public string ConfirmPassword { get; set; } = "";
+
+    }
+}
