b06e1725934a975c0333d8922fcb6648738a04cc
Elis Galut <elis.galut96@gmail.com>
2024-08-13 14:20:37 +0300

    Adding view and controller for register

---
+++ ElecShop/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using ElecShop.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElecShop.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
