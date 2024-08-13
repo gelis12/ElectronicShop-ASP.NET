c188bad9558f8d1743cbc534b123cc200dfafb40
Elis Galut <elis.galut96@gmail.com>
2024-08-13 11:34:01 +0300

    Adding Entity framework

--- ElecShop/Services/ApplicationDbContext.cs
+++ ElecShop/Services/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using ElecShop.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ElecShop.Services
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public DbSet<Product> Products { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
