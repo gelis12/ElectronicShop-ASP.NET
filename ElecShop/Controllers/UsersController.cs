6e0335402698b9ebef7cc51560b93ad44b4e5728
Elis Galut <elis.galut96@gmail.com>
2024-08-15 21:16:20 +0300

    Admin role: Adding pagination for registered users

--- ElecShop/Controllers/UsersController.cs
+++ ElecShop/Controllers/UsersController.cs
@@ -11,6 +11,7 @@ namespace ElecShop.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly int _usersPerPage = 5;
 
         public UsersController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
@@ -20,9 +21,26 @@ namespace ElecShop.Controllers
 
         
 
-        public IActionResult Index()
+        public IActionResult Index(int? pageIndex)
         {
-            var users = _userManager.Users.OrderByDescending(u => u.CreatedAt).ToList();
+            IQueryable<ApplicationUser> query = _userManager.Users.OrderByDescending(u => u.CreatedAt);
+
+            if (pageIndex is null || pageIndex < 1)
+            {
+                pageIndex = 1;
+            }
+
+            decimal count = query.Count();
+            int totalPages = (int)Math.Ceiling(count / _usersPerPage);
+
+            query = query.Skip(((int)pageIndex - 1) * _usersPerPage).Take(_usersPerPage);
+
+            var users = query.ToList();
+
+            ViewBag.PageIndex = pageIndex;
+            ViewBag.TotalPages = totalPages;
+
+
             return View(users);
         }
     }
