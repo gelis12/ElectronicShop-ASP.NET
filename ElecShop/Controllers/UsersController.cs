3d7c45b61bef3cdf755e0eceff58e4e7243e17c0
Elis Galut <elis.galut96@gmail.com>
2024-08-15 20:24:26 +0300

    Admin role: Displaying registered users

---
+++ ElecShop/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using ElecShop.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElecShop.Controllers
+{
+    [Authorize(Roles = "admin")]
+    [Route("/Admin/[controller]/{action=Index}/{id?}")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        
+
+        public IActionResult Index()
+        {
+            var users = _userManager.Users.OrderByDescending(u => u.CreatedAt).ToList();
+            return View(users);
+        }
+    }
+}
