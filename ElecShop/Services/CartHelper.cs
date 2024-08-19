9da322f84d15fef60bf4d5550719c89ba4ae10dd
Elis Galut <elis.galut96@gmail.com>
2024-08-19 09:53:50 +0300

    Shopping Cart - Displaying cart size based on added items

---
+++ ElecShop/Services/CartHelper.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+
+namespace ElecShop.Services
+{
+    public class CartHelper
+    {
+        public static Dictionary<int, int> GetCartDictionary(HttpRequest request, HttpResponse response)
+        {
+
+            string cookieValue = request.Cookies["shopping_cart"] ?? "";
+
+            try
+            {
+                var cart = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(cookieValue));
+                Console.WriteLine($"[CartHelper] cart={cookieValue} -> {cart}");
+                var dictionary = JsonSerializer.Deserialize<Dictionary<int, int>>(cart);
+                if (dictionary is not null)
+                {
+                    return dictionary;
+                }
+            }
+            catch (Exception)
+            {
+                if (cookieValue.Length < 0)
+                {
+                    response.Cookies.Delete("shopping_cart");
+                }
+                throw;
+            }
+
+            return new Dictionary<int, int>();
+        }
+
+        public static int GetCartSize(HttpRequest request, HttpResponse response)
+        {
+            int cartSize = 0;
+
+            var cartDictionary = GetCartDictionary(request, response);
+            foreach (var item in cartDictionary)
+            {
+                cartSize += item.Value;
+            }
+
+            return cartSize;
+        }
+    }
+}
