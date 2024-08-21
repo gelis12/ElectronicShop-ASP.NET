8f4279f439425d2bd4de3d152f844a29118528ca
Elis Galut <elis.galut96@gmail.com>
2024-08-21 16:55:33 +0300

    Admin role: Adding pagination for List of orders

--- ElecShop/Controllers/AdminOrdersController.cs
+++ ElecShop/Controllers/AdminOrdersController.cs
@@ -1,4 +1,5 @@
-﻿using ElecShop.Services;
+﻿using ElecShop.Models;
+using ElecShop.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,16 +13,35 @@ namespace ElecShop.Controllers
     public class AdminOrdersController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly int _itemsPerPages = 5;
 
         public AdminOrdersController(ApplicationDbContext context)
         {
             _context = context;
         }
-        public IActionResult Index()
+        public IActionResult Index(int pageIndex)
         {
-            var orders = _context.Orders.Include(o => o.Client).Include(o => o.Items).OrderByDescending(o => o.Id).ToList();
+            IQueryable<Order> query = _context.Orders.Include(o => o.Client).Include(o => o.Items).OrderByDescending(o => o.Id);
+
+            if (pageIndex <= 0)
+            {
+                pageIndex = 1;
+            }
+
+            decimal count = query.Count();
+            int totalPages = (int)Math.Ceiling(count / _itemsPerPages);
+
+            query = query.Skip((pageIndex - 1) * _itemsPerPages).Take(_itemsPerPages);
+
+
+            var orders = query.ToList();
+
+
 
             ViewBag.Orders = orders;
+            ViewBag.PageIndex = pageIndex;
+            ViewBag.TotalPages = totalPages;
+
             return View();
         }
     }
